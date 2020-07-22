9f55c1d1986020718e067a2e9fd1c72b548a1ee0
Websites.ca <accounts@websites.ca>
2020-07-21 22:50:53 -0400

    not correct, needs to distinguish between CW and CCW somehow

--- client/js/ui/MaskControls.js
+++ client/js/ui/MaskControls.js
@@ -73,15 +73,38 @@ class MaskControls {
       path = paper.path(pathStr);
       path.attr({stroke:"blue", strokeWidth:1, fill:"red", opacity:0.3});
     }
-    function canonical(items) {
-      if (typeof(items[0]) == 'object') { //point
-        return items.map(i => {return {x:i.x, y:i.y}})
-      } else { //command
+    function canonicalPoints(items) {
+      console.log("canonical", items, items[0], typeof(items[0]))
+      if (Array.isArray(items[0])) { //command
         return items.map(i => {
           if (i[0] == 'M') return {x:i[1], y:i[2]}
           else return {x:i[1], y:i[2]}
         })
+      } else { //point
+        return items.map(i => {return {x:Math.round(i.x), y:Math.round(i.y)}})
       }
+    }
+    function findMinX(y, points) {
+      var minPoint = null;
+      for (var p in points) {
+        if (points[p].y == y) {
+          if ((minPoint == null) || (points[p].x < minPoint.x)) {
+            minPoint = p;
+          }
+        }
+      }
+      return minPoint;
+    }
+    function findMinY(x, points) {
+      var minPoint = null;
+      for (var p in points) {
+        if (points[p].x == x) {
+          if ((minPoint == null) || (points[p].y < minPoint.y)) {
+            minPoint = p;
+          }
+        }
+      }
+      return minPoint;
     }
     function commands2string(commands) {
       return commands.reduce((s,c) => {
@@ -92,14 +115,38 @@ class MaskControls {
         return s;
       }, "");
     }
+    function perimeter(points) {
+      var ordered = [];
+      var currentPoint = points[0];
+
+      while (points.length > 0) {
+        var xi = findMinX(currentPoint.y, points);
+        var p = points.splice(xi, 1);
+        ordered.push(p[0]);
+        currentPoint = p;
+
+        var yi = findMinY(currentPoint.x, points);
+        var p = points.splice(yi, 1);
+        ordered.push(p[0]);
+        currentPoint = p;
+      }
+      return ordered;
+    }
+    function flattenPoints(points) {
+      return points.reduce((l, p) => {
+        l.push(p.x);
+        l.push(p.y);
+      }, []);
+    }
     function union(p1, p2) {
       var commands1 = Snap.path.map(p1, p1.transform().localMatrix);
-      var points = Snap.path.intersection(p1, p2);
+      var intersectionPoints = Snap.path.intersection(p1, p2);
       var commands2 = Snap.path.map(p2, p2.transform().localMatrix);
-      console.log(commands1, points, commands2);
 
-      if (points.length > 0) {
-        
+      if (intersectionPoints.length > 0) {
+        var points = [canonicalPoints(commands1), canonicalPoints(intersectionPoints), canonicalPoints(commands2)].flat();
+        var ordered = perimeter(points);
+        return ordered;
       } else {
         return commands2string(commands1) + commands2string(commands2);
       }
@@ -120,10 +167,15 @@ class MaskControls {
       finalPath.remove();
 
       //var maskPathStr = paper.union(maskPath, finalPath);
-      //var maskPathStr = Snap.path.intersection(maskPath, finalPath);
-      var maskPathStr = union(maskPath, finalPath);
-      console.log("maskPathStr", maskPathStr);
-      maskPath = paper.path(maskPathStr).attr({stroke:"red", strokeWidth:1, fill:"yellow", opacity:0.2})
+      //var maskPathStr = union(maskPath, finalPath);
+      //console.log("maskPathStr", maskPathStr);
+      //maskPath = paper.path(maskPathStr).attr({stroke:"red", strokeWidth:1, fill:"yellow", opacity:0.2})
+
+      var points = union(maskPath, finalPath);
+      console.log("ordered points", points);
+      points = points.map(p=>[p.x,p.y]).flat()
+      console.log("flattened ordered points", points);
+      maskPath = paper.polyline(points).attr({stroke:"red", strokeWidth:1, fill:"yellow", opacity:0.2})
     }
     paper.drag(onmove, onstart, onend);
   }
