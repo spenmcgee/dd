078ef1d7f90c04440418911796fed67b4077ea12
Websites.ca <accounts@websites.ca>
2020-07-21 23:46:18 -0400

    this adds mask rects and *appears* to union them using the white/black blending

--- client/js/ui/MaskControls.js
+++ client/js/ui/MaskControls.js
@@ -63,119 +63,26 @@ class MaskControls {
     var path = null;
     var pathStr = null;
     var maskPath = null;
+    var mask = null, maskbg = null;
+    var rects = [];
 
     function onstart(x, y, e) {
-      //noop
+      rect = this.paper.rect(x,y,1,1).attr({fill:'red', opacity:0.3});
     }
     function onmove(dx, dy, x, y, e) {
-      if (path) path.remove();
-      pathStr = `M ${x-dx} ${y-dy} l ${dx} 0 l 0 ${dy} l ${-1*dx} 0 l 0 ${-1*dy}`;
-      path = paper.path(pathStr);
-      path.attr({stroke:"blue", strokeWidth:1, fill:"red", opacity:0.3});
-    }
-    function canonicalPoints(items) {
-      console.log("canonical", items, items[0], typeof(items[0]))
-      if (Array.isArray(items[0])) { //command
-        return items.map(i => {
-          if (i[0] == 'M') return {x:i[1], y:i[2]}
-          else return {x:i[1], y:i[2]}
-        })
-      } else { //point
-        return items.map(i => {return {x:Math.round(i.x), y:Math.round(i.y)}})
-      }
-    }
-    function findMinX(y, points) {
-      var minPoint = null;
-      for (var p in points) {
-        if (points[p].y == y) {
-          if ((minPoint == null) || (points[p].x < minPoint.x)) {
-            minPoint = p;
-          }
-        }
-      }
-      return minPoint;
-    }
-    function findMinY(x, points) {
-      var minPoint = null;
-      for (var p in points) {
-        if (points[p].x == x) {
-          if ((minPoint == null) || (points[p].y < minPoint.y)) {
-            minPoint = p;
-          }
-        }
-      }
-      return minPoint;
-    }
-    function commands2string(commands) {
-      return commands.reduce((s,c) => {
-        if (c[0] == 'M')
-          s += `M ${c[1]} ${c[2]} `;
-        else
-          s += `L ${c[5]} ${c[6]} `;
-        return s;
-      }, "");
-    }
-    function perimeter(points) {
-      var ordered = [];
-      var currentPoint = points[0];
-
-      while (points.length > 0) {
-        var xi = findMinX(currentPoint.y, points);
-        var p = points.splice(xi, 1);
-        ordered.push(p[0]);
-        currentPoint = p;
-
-        var yi = findMinY(currentPoint.x, points);
-        var p = points.splice(yi, 1);
-        ordered.push(p[0]);
-        currentPoint = p;
-      }
-      return ordered;
-    }
-    function flattenPoints(points) {
-      return points.reduce((l, p) => {
-        l.push(p.x);
-        l.push(p.y);
-      }, []);
-    }
-    function union(p1, p2) {
-      var commands1 = Snap.path.map(p1, p1.transform().localMatrix);
-      var intersectionPoints = Snap.path.intersection(p1, p2);
-      var commands2 = Snap.path.map(p2, p2.transform().localMatrix);
-
-      if (intersectionPoints.length > 0) {
-        var points = [canonicalPoints(commands1), canonicalPoints(intersectionPoints), canonicalPoints(commands2)].flat();
-        var ordered = perimeter(points);
-        return ordered;
-      } else {
-        return commands2string(commands1) + commands2string(commands2);
-      }
-
+      var attr = {width:Math.abs(dx), height:Math.abs(dy)};
+      if (dx < 0) attr.x = x;
+      if (dy < 0) attr.y = y;
+      rect.attr(attr);
     }
     function onend(e) {
-      path.remove();
-
-      var finalPath = paper.path(pathStr);
-      finalPath.attr({stroke:"blue", strokeWidth:1, fill:"red", opacity:0.4});
-
-      if (!maskPath) {
-        maskPath = finalPath;
-        return;
-      }
-
-      maskPath.remove();
-      finalPath.remove();
-
-      //var maskPathStr = paper.union(maskPath, finalPath);
-      //var maskPathStr = union(maskPath, finalPath);
-      //console.log("maskPathStr", maskPathStr);
-      //maskPath = paper.path(maskPathStr).attr({stroke:"red", strokeWidth:1, fill:"yellow", opacity:0.2})
-
-      var points = union(maskPath, finalPath);
-      console.log("ordered points", points);
-      points = points.map(p=>[p.x,p.y]).flat()
-      console.log("flattened ordered points", points);
-      maskPath = paper.polyline(points).attr({stroke:"red", strokeWidth:1, fill:"yellow", opacity:0.2})
+      rect.attr({fill:'white', opacity:1});
+      rects.push(rect);
+      rect.remove();
+      mask = paper.g(...rects);
+      var bb = mask.getBBox();
+      if (maskbg) maskbg.remove();
+      maskbg = paper.rect(bb).attr({fill:'yellow', mask:mask, opacity:0.5})
     }
     paper.drag(onmove, onstart, onend);
   }
